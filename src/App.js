439130dec5dde1907fe73238b238eea7301a6263
Justin <justinadams758@gmail.com>
2022-04-12 23:37:40 -0600

    crew members showing..

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import { BankSelection } from './Components/BankSelection';
+import { CrewSelection } from './Components/CrewSelection';
 
 function App() {
   return (
     <div className="App">
       <BankSelection/>
+      <CrewSelection/>
       
     </div>
   );
