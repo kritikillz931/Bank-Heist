d0a823a410b2d06e01aeab1cbc8adceb5b2fbfc9
Justin <justinadams758@gmail.com>
2022-04-12 19:10:04 -0600

    bank selectionsworking

---
+++ src/Components/BankSelection.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Bank1 from "../Images/Bank1.png"
+import Bank2 from "../Images/Bank2.png"
+import Bank3 from "../Images/Bank3.png"
+import "../Components/BankSelection.css"
+
+export const BankSelection = () => {
+    let bank1 = {
+        img: Bank1,
+        difficulty: Math.floor(Math.random(1) * 10 + 1)
+    }
+    let bank2 = {
+        img: Bank2,
+        difficulty: Math.floor(Math.random(1) * 10 + 1)
+    }
+    let bank3 = {
+        img: Bank3,
+        difficulty: Math.floor(Math.random() * 10 + 1)
+    }
+
+    let banks = [bank1, bank2, bank3]
+
+    const handleBankSelection = (bank) => {
+        console.log(bank)
+        sessionStorage.setItem("bankDifficulty", bank.difficulty)
+        sessionStorage.setItem("bankImg", bank.img)
+        
+    }
+    return (
+        <>
+        <div>
+            <h3>PICK A BANK</h3>
+        </div>
+        <div className="imageContainer">
+            {banks.map((bank) => {
+                return (
+                    <div >
+                        <img onClick={() => handleBankSelection(bank)} className="bankImg" src={bank.img}/>
+                        <h6>Bank Difficulty: {bank.difficulty}</h6>
+                    </div>
+                )
+            })}
+        </div>
+            </>
+    )
+}
