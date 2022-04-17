7b7e24972778943a0523e735e1d9b28f8c4b4c95
Justin <justinadams758@gmail.com>
2022-04-17 11:58:34 -0600

    saving progress

--- src/Components/CrewSelection.js
+++ src/Components/CrewSelection.js
@@ -1,24 +1,32 @@
 import React, { useState } from "react";
-import { robberName } from "./CrewDetails"
+import { robberDetails } from "./CrewDetails"
 import { robberAvatar } from "./CrewDetails"
 import "./CrewSelection.css"
-export const CrewSelection = () => {
+
+export const CrewSelection = ({}) => {
 
 
     return (
         <>
             <h3>PICK YOUR CREW</h3>
-            <div className="crewContainer">
-                {robberName.map((robber) => {
+            <div className="crewContainer" >
+                {robberDetails.map((robber) => {
                     return (
                         <>
-                            <div className="crewCards">
+                            <div className="crewCards" >
                                 <div className="crewImages">
                                     {robberAvatar}
-                                </div>
                                 <div className="crewDetails">
-                                    {robber}
+                                    {robber.name}
                                     <div>
+                                    Health: {robber.health}
+                                    </div>
+                                    <div>
+                                    Skill Level: {robber.skillLevel}
+                                    </div>
+                                    <div>
+                                    Cost To Hire: {robber.costToHire}
+                                </div>
                                         <input value={robber} type="checkbox"/>
                                     </div>
                                 </div>
---
+++ src/Components/EquipmentSelection.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const EquipmentSelection = () => {
+return (
+    <>
+        <h3>PICK YOUR EQUIPMENT</h3>
+        <div className="equipmentContainer">
+            <div className="equipmentImages">
+
+            </div>
+        </div>
+    </>
+ 
+)
+}
