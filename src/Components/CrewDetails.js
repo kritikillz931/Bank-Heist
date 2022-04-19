10c9f63088d08aa1bcf3b8d3955b64c1f9049d8e
Justin <justinadams758@gmail.com>
2022-04-19 12:31:21 -0600

    saving multiple checkboxes successfully

--- src/Components/CrewDetails.js
+++ src/Components/CrewDetails.js
@@ -35,67 +35,88 @@ export let costToHireGenerator8 = Math.round(Math.random() * (10000 - 1000) + 10
 export let costToHireGenerator9 = Math.round(Math.random() * (10000 - 1000) + 1000)
 export let costToHireGenerator10 = Math.round(Math.random() * (10000 - 1000) + 1000)
 
+let effectivenessGenerator = Math.round(Math.random() * (10 - 1) + 1)
+let effectivenessGenerator2 = Math.round(Math.random() * (10 - 1) + 1)
+let effectivenessGenerator3 = Math.round(Math.random() * (10 - 1) + 1)
+let effectivenessGenerator4 = Math.round(Math.random() * (10 - 1) + 1)
+let effectivenessGenerator5 = Math.round(Math.random() * (10 - 1) + 1)
+let effectivenessGenerator6 = Math.round(Math.random() * (10 - 1) + 1)
+let effectivenessGenerator7 = Math.round(Math.random() * (10 - 1) + 1)
+let effectivenessGenerator8 = Math.round(Math.random() * (10 - 1) + 1)
+let effectivenessGenerator9 = Math.round(Math.random() * (10 - 1) + 1)
+let effectivenessGenerator10 = Math.round(Math.random() * (10 - 1) + 1)
+
 export let robberDetails = [
     
         {
             "name": "Radcliff the Fool",
             "health": healthGenerator,
             "skillLevel": skillGenerator,
-            "costToHire": costToHireGenerator
+            "costToHire": costToHireGenerator,
+            "effictiveness": effectivenessGenerator
         },
         {
             "name": "Hamilton Blackjack",
             "health": healthGenerator2,
             "skillLevel": skillGenerator2,
-            "costToHire": costToHireGenerator2
+            "costToHire": costToHireGenerator2,
+            "effictiveness": effectivenessGenerator2
         },
         {
             "name": "Tyrell Shades",
             "health": healthGenerator3,
             "skillLevel": skillGenerator3,
-            "costToHire": costToHireGenerator3
+            "costToHire": costToHireGenerator3,
+            "effictiveness": effectivenessGenerator3
         },
         {
             "name": "Mad Man Paul",
             "health": healthGenerator4,
             "skillLevel": skillGenerator4,
-            "costToHire": costToHireGenerator4
+            "costToHire": costToHireGenerator4,
+            "effictiveness": effectivenessGenerator4
         },
         {
             "name": "Fast Fingers Tomkin",
             "health": healthGenerator5,
             "skillLevel": skillGenerator5,
-            "costToHire": costToHireGenerator5
+            "costToHire": costToHireGenerator5,
+            "effictiveness": effectivenessGenerator5
         },
         {
             "name": "Whispering Forbes",
             "health": healthGenerator6,
             "skillLevel": skillGenerator6,
-            "costToHire": costToHireGenerator6
+            "costToHire": costToHireGenerator6,
+            "effictiveness": effectivenessGenerator6
         },
         {
             "name": "Nick Danger",
             "health": healthGenerator7,
             "skillLevel": skillGenerator7,
-            "costToHire": costToHireGenerator7
+            "costToHire": costToHireGenerator7,
+            "effictiveness": effectivenessGenerator7
         },
         {
             "name": "Alfie Muscles",
             "health": healthGenerator8,
             "skillLevel": skillGenerator8,
-            "costToHire": costToHireGenerator8
+            "costToHire": costToHireGenerator8,
+            "effictiveness": effectivenessGenerator8
         },
         {
             "name": "Blackjack Julian",
             "health": healthGenerator9,
             "skillLevel": skillGenerator9,
-            "costToHire": costToHireGenerator9
+            "costToHire": costToHireGenerator9,
+            "effictiveness": effectivenessGenerator9
         },
         {
             "name": "Bulletproof Bryant",
             "health": healthGenerator10,
             "skillLevel": skillGenerator10,
-            "costToHire": costToHireGenerator10
+            "costToHire": costToHireGenerator10,
+            "effictiveness": effectivenessGenerator10
         }
     
 ]
--- src/Components/CrewSelection.js
+++ src/Components/CrewSelection.js
@@ -1,9 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { robberDetails } from "./CrewDetails"
 import { robberAvatar } from "./CrewDetails"
 import "./CrewSelection.css"
 
-export const CrewSelection = ({}) => {
+export const CrewSelection = ({ }) => {
+    let crew = []
+    useEffect(() => {
+
+    }, [crew])
+
+    const handleInputChange = (event, robber) => {
+        
+        if(event.target.checked === true) {
+            crew.push(robber)
+        } 
+            if (event.target.checked === false) {
+                var i = 0
+                while(i < crew.length) {
+                    if(crew[i] === robber) {
+                        crew.splice(i, 1)
+                    } else {
+                        ++i
+                    }
+                } 
+            }
+        console.log(crew)
+    }
+
+    const handleOnClick = () => {
+        if(crew) {
+            let finalCrew = []
+            finalCrew.push(crew)
+            console.log(finalCrew)
+        }
+    }
 
     return (
         <>
@@ -15,26 +45,26 @@ export const CrewSelection = ({}) => {
                             <div className="crewCards" >
                                 <div className="crewImages">
                                     {robberAvatar}
-                                <div className="crewDetails">
-                                    {robber.name}
-                                    <div>
-                                    Health: {robber.health}
-                                    </div>
-                                    <div>
-                                    Skill Level: {robber.skillLevel}
-                                    </div>
-                                    <div>
-                                    Cost To Hire: {robber.costToHire}
-                                </div>
-                                        <input value={robber} type="checkbox"/>
+                                    <div className="crewDetails">
+                                        {robber.name}
+                                        <div>
+                                            Health: {robber.health}
+                                        </div>
+                                        <div>
+                                            Skill Level: {robber.skillLevel}
+                                        </div>
+                                        <div>
+                                            Cost To Hire: {robber.costToHire}
+                                        </div>
+                                        <input onClick={handleOnClick} name="hiredCrew" value={robber.name} onChange={(e) => handleInputChange(e, robber)} type="checkbox"  />
                                     </div>
                                 </div>
                             </div>
                         </>
 
-)
+                    )
 
-})}
+                })}
             </div>
         </>
 
